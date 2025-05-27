5c37596ddc616d7e8d5657835a8e7c5f4a29c08b
Evgeniy Kozlov <dsuchka@gmail.com>
2025-05-27 23:45:48 +0300

    WIP (common/max_scalar: support aligned)

--- src/common.c
+++ src/common.c
@@ -10,31 +10,68 @@ void assure_error_desc_empty(char **error_desc)
     }
 }
 
-void xor_memory_region(uint8_t *dptr, uint8_t *mask, size_t size)
+void __xor_memory_region_simple(uint8_t *dptr, uint8_t *mask, size_t size)
+{
+#if (MAX_SCALAR_SIZE >= 64)
+    // process remaining quad words
+    while (size > sizeof(uint64_t)) {
+        register uint64_t v0 = *((uint64_t*)dptr);
+        register uint64_t m0 = *((uint64_t*)mask);
+
+        *((uint64_t*)dptr) = v0 ^ m0;
+
+        size -= sizeof(uint64_t);
+        dptr += sizeof(uint64_t);
+        mask += sizeof(uint64_t);
+    }
+#endif
+
+    // process remaining double words
+#if (MAX_SCALAR_SIZE >= 64)
+    if
+#else
+    while
+#endif
+    (size > sizeof(uint32_t)) {
+        register uint32_t v0 = *((uint32_t*)dptr);
+        register uint32_t m0 = *((uint32_t*)mask);
+
+        *((uint32_t*)dptr) = v0 ^ m0;
+
+        size -= sizeof(uint32_t);
+        dptr += sizeof(uint32_t);
+        mask += sizeof(uint32_t);
+    }
+
+    // process remaining bytes
+    while (size-- > 0) {
+        *(dptr++) ^= *(mask++);
+    }
+
+    return;
+}
+
+#if (MAX_SCALAR_SIZE > 64)
+
+void __xor_memory_region_aligned_both(uint8_t *dptr, uint8_t *mask, size_t size)
 {
     // unroll loop for 4 x max scalars
     while (size >= sizeof(max_scalar_t) * 4) {
-        {
-            register max_scalar_t s0 = MAX_SCALAR_LOAD(dptr + sizeof(max_scalar_t) * 0);
-            register max_scalar_t s1 = MAX_SCALAR_LOAD(dptr + sizeof(max_scalar_t) * 1);
+        register max_scalar_t s0 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 0);
+        register max_scalar_t s1 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 1);
+        register max_scalar_t m0 = MAX_SCALAR_LOAD_ALIGNED(mask + sizeof(max_scalar_t) * 0);
+        register max_scalar_t m1 = MAX_SCALAR_LOAD_ALIGNED(mask + sizeof(max_scalar_t) * 1);
 
-            register max_scalar_t m0 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 0);
-            register max_scalar_t m1 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 1);
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 0, MAX_SCALAR_XOR(s0, m0));
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 1, MAX_SCALAR_XOR(s1, m1));
 
-            MAX_SCALAR_STORE(dptr + sizeof(max_scalar_t) * 0, MAX_SCALAR_XOR(s0, m0));
-            MAX_SCALAR_STORE(dptr + sizeof(max_scalar_t) * 1, MAX_SCALAR_XOR(s1, m1));
-        }
+        register max_scalar_t s2 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 2);
+        register max_scalar_t s3 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 3);
+        register max_scalar_t m2 = MAX_SCALAR_LOAD_ALIGNED(mask + sizeof(max_scalar_t) * 2);
+        register max_scalar_t m3 = MAX_SCALAR_LOAD_ALIGNED(mask + sizeof(max_scalar_t) * 3);
 
-        {
-            register max_scalar_t s2 = MAX_SCALAR_LOAD(dptr + sizeof(max_scalar_t) * 2);
-            register max_scalar_t s3 = MAX_SCALAR_LOAD(dptr + sizeof(max_scalar_t) * 3);
-
-            register max_scalar_t m2 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 2);
-            register max_scalar_t m3 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 3);
-
-            MAX_SCALAR_STORE(dptr + sizeof(max_scalar_t) * 2, MAX_SCALAR_XOR(s2, m2));
-            MAX_SCALAR_STORE(dptr + sizeof(max_scalar_t) * 3, MAX_SCALAR_XOR(s3, m3));
-        }
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 2, MAX_SCALAR_XOR(s2, m2));
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 3, MAX_SCALAR_XOR(s3, m3));
 
         size -= sizeof(max_scalar_t) * 4;
         dptr += sizeof(max_scalar_t) * 4;
@@ -43,19 +80,105 @@ void xor_memory_region(uint8_t *dptr, uint8_t *mask, size_t size)
 
     // process remaining max scalars
     while (size >= sizeof(max_scalar_t)) {
-        register max_scalar_t s0 = MAX_SCALAR_LOAD(dptr + sizeof(max_scalar_t) * 0);
-        register max_scalar_t m0 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 0);
+        register max_scalar_t s0 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 0);
+        register max_scalar_t m0 = MAX_SCALAR_LOAD_ALIGNED(mask + sizeof(max_scalar_t) * 0);
 
-        MAX_SCALAR_STORE(dptr + sizeof(max_scalar_t) * 0, MAX_SCALAR_XOR(s0, m0));
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 0, MAX_SCALAR_XOR(s0, m0));
 
         size -= sizeof(max_scalar_t);
         dptr += sizeof(max_scalar_t);
         mask += sizeof(max_scalar_t);
     }
 
-    // process remaining bytes
-    while (size-- > 0) {
-        *(dptr++) ^= *(mask++);
+    // process remaining simply
+    if (size) {
+        __xor_memory_region_simple(dptr, mask, size);
     }
+
+    return;
+}
+
+void __xor_memory_region_aligned_dptr(uint8_t *dptr, uint8_t *mask, size_t size)
+{
+    // unroll loop for 4 x max scalars
+    while (size >= sizeof(max_scalar_t) * 4) {
+        register max_scalar_t s0 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 0);
+        register max_scalar_t s1 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 1);
+        register max_scalar_t m0 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 0);
+        register max_scalar_t m1 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 1);
+
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 0, MAX_SCALAR_XOR(s0, m0));
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 1, MAX_SCALAR_XOR(s1, m1));
+
+        register max_scalar_t s2 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 2);
+        register max_scalar_t s3 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 3);
+        register max_scalar_t m2 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 2);
+        register max_scalar_t m3 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 3);
+
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 2, MAX_SCALAR_XOR(s2, m2));
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 3, MAX_SCALAR_XOR(s3, m3));
+
+        size -= sizeof(max_scalar_t) * 4;
+        dptr += sizeof(max_scalar_t) * 4;
+        mask += sizeof(max_scalar_t) * 4;
+    }
+
+    // process remaining max scalars
+    while (size >= sizeof(max_scalar_t)) {
+        register max_scalar_t s0 = MAX_SCALAR_LOAD_ALIGNED(dptr + sizeof(max_scalar_t) * 0);
+        register max_scalar_t m0 = MAX_SCALAR_LOAD(mask + sizeof(max_scalar_t) * 0);
+
+        MAX_SCALAR_STORE_ALIGNED(dptr + sizeof(max_scalar_t) * 0, MAX_SCALAR_XOR(s0, m0));
+
+        size -= sizeof(max_scalar_t);
+        dptr += sizeof(max_scalar_t);
+        mask += sizeof(max_scalar_t);
+    }
+
+    // process remaining simply
+    if (size) {
+        __xor_memory_region_simple(dptr, mask, size);
+    }
+
+    return;
+}
+
+#endif // (MAX_SCALAR_SIZE > 64)
+
+
+void xor_memory_region(uint8_t *dptr, uint8_t *mask, size_t size)
+{
+    size_t dptr_unaligned_tail = (size_t)dptr & (~MAX_SCALAR_ALIGN_MASK);
+
+    // do align (process) if there some portion of unaligned bytes
+    if (dptr_unaligned_tail) {
+        size_t count = MAX_SCALAR_BYTES - dptr_unaligned_tail;
+
+        if (size <= count) {
+            __xor_memory_region_simple(dptr, mask, size);
+
+            return;
+        }
+
+        __xor_memory_region_simple(dptr, mask, count);
+
+        size -= count;
+        dptr += count;
+        mask += count;
+    }
+
+#if (MAX_SCALAR_SIZE > 64)
+    size_t mask_unaligned_tail = (size_t)mask & (~MAX_SCALAR_ALIGN_MASK);
+
+    if (mask_unaligned_tail) {
+        __xor_memory_region_aligned_dptr(dptr, mask, size);
+    } else {
+        __xor_memory_region_aligned_both(dptr, mask, size);
+    }
+#else
+    __xor_memory_region_simple(dptr, mask, size);
+#endif
+
+    return;
 }
 
--- src/max_scalar.h
+++ src/max_scalar.h
@@ -1,40 +1,94 @@
 #ifndef __MAX_SCALAR_H
 #define __MAX_SCALAR_H
 
+
+//
+//  AVX2
+//
+
 #if defined(__AVX2__)
+
+# define MAX_SCALAR_SIZE                        256
 
 # include <immintrin.h>
 
-typedef __m256i max_scalar_t;
+typedef __m256i scalar256_t;
 
-# define MAX_SCALAR_LOAD(ptr)           _mm256_loadu_si256((__m256i*)(ptr))
-# define MAX_SCALAR_XOR(a, b)           _mm256_xor_si256((a), (b))
-# define MAX_SCALAR_STORE(ptr, val)     _mm256_storeu_si256((__m256i*)(ptr), (val))
+# define SCALAR256_LOAD(ptr)                    _mm256_loadu_si256((__m256i*)(ptr))
+# define SCALAR256_LOAD_ALIGNED(ptr)            _mm256_load_si256((__m256i*)(ptr))
+# define SCALAR256_STORE(ptr, val)              _mm256_storeu_si256((__m256i*)(ptr), (val))
+# define SCALAR256_STORE_ALIGNED(ptr, val)      _mm256_store_si256((__m256i*)(ptr), (val))
+# define SCALAR256_XOR(a, b)                    _mm256_xor_si256((a), (b))
 
-#elif defined(__SSE2__)
+#endif // __AVX2__
+
+
+//
+//  SSE2
+//
+
+#if defined(__SSE2__)
+
+# ifndef MAX_SCALAR_SIZE
+#  define MAX_SCALAR_SIZE                       128
+# endif // MAX_SCALAR_SIZE
 
 # include <emmintrin.h>
 
+typedef __m128i scalar128_t;
+
+# define SCALAR128_LOAD(ptr)                    _mm_loadu_si128((__m128i*)(ptr))
+# define SCALAR128_LOAD_ALIGNED(ptr)            _mm_load_si128((__m128i*)(ptr))
+# define SCALAR128_STORE(ptr, val)              _mm_storeu_si128((__m128i*)(ptr), (val))
+# define SCALAR128_STORE_ALIGNED(ptr, val)      _mm_storeu_si128((__m128i*)(ptr), (val))
+# define SCALAR128_XOR(a, b)                    _mm_xor_si128((a), (b))
+
+#endif // __SSE2__
+
+
+//
+//  MAX_SCALAR
+//
+
+#if (MAX_SCALAR_SIZE == 256)
+
+typedef __m256i max_scalar_t;
+
+# define MAX_SCALAR_LOAD(ptr)                   SCALAR256_LOAD(ptr)
+# define MAX_SCALAR_LOAD_ALIGNED(ptr)           SCALAR256_LOAD_ALIGNED(ptr)
+# define MAX_SCALAR_STORE(ptr, val)             SCALAR256_STORE(ptr, val)
+# define MAX_SCALAR_STORE_ALIGNED(ptr, val)     SCALAR256_STORE_ALIGNED(ptr, val)
+# define MAX_SCALAR_XOR(a, b)                   SCALAR256_XOR(a, b)
+
+#elif (MAX_SCALAR_SIZE == 128)
+
 typedef __m128i max_scalar_t;
 
-# define MAX_SCALAR_LOAD(ptr)           _mm_loadu_si128((__m128i*)(ptr))
-# define MAX_SCALAR_XOR(a, b)           _mm_xor_si128((a), (b))
-# define MAX_SCALAR_STORE(ptr, val)     _mm_storeu_si128((__m128i*)(ptr), (val))
+# define MAX_SCALAR_LOAD(ptr)                   SCALAR128_LOAD(ptr)
+# define MAX_SCALAR_LOAD_ALIGNED(ptr)           SCALAR128_LOAD_ALIGNED(ptr)
+# define MAX_SCALAR_STORE(ptr, val)             SCALAR128_STORE(ptr, val)
+# define MAX_SCALAR_STORE_ALIGNED(ptr, val)     SCALAR128_STORE_ALIGNED(ptr, val)
+# define MAX_SCALAR_XOR(a, b)                   SCALAR128_XOR(a, b)
 
-#else // uint
+#else
 
 # include <stdint.h>
 
-# if __WORDSIZE == 64
+# if (__WORDSIZE == 64)
+#  define MAX_SCALAR_SIZE                       64
 typedef uint64_t max_scalar_t;
 # else
+#  define MAX_SCALAR_SIZE                       32
 typedef uint32_t max_scalar_t;
 # endif // __WORDSIZE == 64
 
-# define MAX_SCALAR_LOAD(ptr)           (*(max_scalar_t*)(ptr))
-# define MAX_SCALAR_XOR(a, b)           ((a) ^ (b))
-# define MAX_SCALAR_STORE(ptr, val)     (*(max_scalar_t*)(ptr) = (val))
+# define MAX_SCALAR_LOAD(ptr)                   (*(max_scalar_t*)(ptr))
+# define MAX_SCALAR_XOR(a, b)                   ((a) ^ (b))
+# define MAX_SCALAR_STORE(ptr, val)             (*(max_scalar_t*)(ptr) = (val))
 
-#endif // __AVX2__/__SSE2__/uint
+#endif // MAX_SCALAR_SIZE 256/128/(64/32)
+
+#define MAX_SCALAR_BYTES                        ((MAX_SCALAR_SIZE) >> 3)
+#define MAX_SCALAR_ALIGN_MASK                   (~((MAX_SCALAR_BYTES) - 1))
 
 #endif // __MAX_SCALAR_H
